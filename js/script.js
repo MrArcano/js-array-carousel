1cca5e719fa8b3939d98a836bd155c9e93166fb3
MrArcano <salvatore-dario@hotmail.it>
2023-09-13 15:14:09 +0200

    [add]: load img js to html

---
+++ js/script.js
@@ -0,0 +1,43 @@
+// carousel image
+const carouselImg = document.querySelector(".carousel-img");
+// button
+const btnUp = document.querySelector(".btn-up") 
+const btnDown = document.querySelector(".btn-down")
+
+// Array di img
+const imgArray = [
+  "img/01.webp",
+  "img/02.webp",
+  "img/03.webp",
+  "img/04.webp",
+  "img/05.webp",
+];
+// -----------------------------------------------
+//  CARICO il div "carousel-img"
+// -----------------------------------------------
+for(let i=0; i<imgArray.length; i++){
+  carouselImg.innerHTML += `<img src="${imgArray[i]}" class="hide img-car">`
+}
+// -----------------------------------------------
+
+// Indice dell'immagine visibile nel carousel
+indexImg = 0;
+
+// Array di elementi html per "img-car"
+const carouselArray = document.getElementsByClassName("img-car")
+console.log(carouselArray);
+
+// Di default rimuovo al primo elemento la classe hide
+carouselArray[0].classList.remove("hide");
+
+// al click del button up
+btnUp.addEventListener("click", function(){
+  console.log("cliccato UP");
+
+});
+
+// al click del button down
+btnDown.addEventListener("click", function(){
+  console.log("cliccato DOWN");
+
+});
